b9632034358d809328647831d2a9bf8aa0d1fae5
hexmarsh <121069920+hexmarsh@users.noreply.github.com>
2023-05-11 13:58:35 -0400

    Update WeaponController.java

--- src/main/java/dev/hexmarsh/controller/WeaponController.java
+++ src/main/java/dev/hexmarsh/controller/WeaponController.java
@@ -1,7 +1,6 @@
 package dev.hexmarsh.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import dev.hexmarsh.model.Weapon;
 import dev.hexmarsh.service.WeaponService;
 
@@ -54,9 +52,6 @@ public class WeaponController {
     {
         return service.getWeaponByAttackType(attackType);
     }
-
-    @GetMapping("/range/{range}")
-
 
     @PutMapping
     public Weapon updateWeapon(@RequestBody Weapon weapon)
